621a78a9518c7b6e292db1a6de4b65f3efa33942
Dhananjai Sharma <dhananjaisharma26@gmail.com>
2021-08-29 12:20:11 +0800

    refactor: changed variable names, added comments

--- dry_principle.cpp
+++ dry_principle.cpp
@@ -1,71 +1,78 @@
 /**
- * This file describes an example to follow the Don't Repeat Yourself
- * (DRY) principle
+ * An example of the Don't Repeat Yourself (DRY) principle
+ *
+ * We would like to write a Worker class which can support 2 types of
+ * functionalities depending on some helper. The simplest solution is to just
+ * write 2 different classes, but if there is reasonable overlap for the 2
+ * versions of the worker, we should make use of the DRY principle.
+ *
+ * In this example, we have 2 helper classes for a Worker class.
+ * Both helpers provide slightly different functionalities to the worker.
+ * I have used dependency injection to pass the helper object to the worker.
+ * In addition, the worker has different parameters depending on the
+ * helper. I have made use of the std::optional feature for the same.
  */
 
 #include <iostream>
 #include <memory>
 #include <optional>
-#include <string>
 #include <vector>
 
-// TODO: Change the variable names, make them generic.
-// TODO: Describe what the problem is, and how we are going to solve it.
-
-class DecoderInterface {
+class HelperInterface {
 public:
-  DecoderInterface() = default;
-  virtual ~DecoderInterface() = default;
-  virtual void decodeDetections(const std::vector<float> &preds,
-                                std::vector<float> &outputs) const = 0;
+  HelperInterface() = default;
+  virtual ~HelperInterface() = default;
+  virtual void compute(const std::vector<float> &inputs,
+                       std::vector<float> &outputs) const = 0;
 };
 
-class DecoderFoo : public DecoderInterface {
+class FooHelper : public HelperInterface {
 public:
-  DecoderFoo() = default;
-  ~DecoderFoo() override {}
+  FooHelper() = default;
+  ~FooHelper() override {}
 
-  void decodeDetections(const std::vector<float> &preds,
-                        std::vector<float> &outputs) const override {
+  void compute(const std::vector<float> &inputs,
+               std::vector<float> &outputs) const override {
     for (size_t i = 1; i < outputs.size(); i++) {
-      outputs[i] += preds[i - 1];
+      outputs[i] += inputs[i - 1];
     }
   }
 };
 
-class DecoderBoo : public DecoderInterface {
+class BooHelper : public HelperInterface {
 public:
-  DecoderBoo() = default;
-  ~DecoderBoo() override {}
+  BooHelper() = default;
+  ~BooHelper() override {}
 
-  void decodeDetections(const std::vector<float> &preds,
-                        std::vector<float> &outputs) const override {
+  void compute(const std::vector<float> &inputs,
+               std::vector<float> &outputs) const override {
     for (size_t i = 1; i < outputs.size(); i++) {
-      outputs[i] += preds[i - 1] * 2;
+      outputs[i] += inputs[i - 1] * 2;
     }
   }
 };
 
-class Detector {
+class Worker {
 public:
   struct Params {
     int height{};
     int width{};
-    std::optional<bool> frontFacing;
+    std::optional<bool> depth;
   };
 
-  Detector(Params params, std::unique_ptr<DecoderInterface> decoder)
-      : params_{std::move(params)}, decoder_{std::move(decoder)} {}
+  // Dependency injection for HelperInterface
+  Worker(Params params, std::unique_ptr<HelperInterface> helper)
+      : params_{std::move(params)}, helper_{std::move(helper)} {}
 
-  std::vector<float> process(const std::vector<float> &input) const {
+  std::vector<float> doSomething(const std::vector<float> &input) const {
     auto output{input};
-    decoder_->decodeDetections(input, output);
+    helper_->compute(input, output);
     return output;
   }
 
 private:
   Params params_;
-  std::unique_ptr<DecoderInterface> decoder_;
+  std::unique_ptr<HelperInterface> helper_;
 };
 
 void printVec(const std::vector<float> &vec) {
@@ -76,18 +83,22 @@ void printVec(const std::vector<float> &vec) {
 }
 
 int main() {
-  Detector::Params paramsFoo{100, 200};
-  Detector::Params paramsBoo{100, 200, true};
-  auto decoderFoo = std::make_unique<DecoderFoo>();
-  auto decoderBoo = std::make_unique<DecoderBoo>();
-  Detector foo{paramsFoo, std::move(decoderFoo)};
-  Detector boo{paramsBoo, std::move(decoderBoo)};
+  Worker::Params fooParams{100, 200};
+  Worker::Params booParams{100, 200, /*optional*/ 130};
+
+  auto fooHelper = std::make_unique<FooHelper>();
+  auto booHelper = std::make_unique<BooHelper>();
+
+  Worker fooWorker{fooParams, std::move(fooHelper)};
+  Worker booWorker{booParams, std::move(booHelper)};
 
   const std::vector<float> input{1, 2, 3, 4};
 
-  auto fooOut = foo.process(input);
-  auto booOut = boo.process(input);
+  auto fooOut = fooWorker.doSomething(input);
+  auto booOut = booWorker.doSomething(input);
 
+  // The expected result would be different for both outputs, depending on
+  // the injected helper object.
   printVec(fooOut);
   printVec(booOut);
 
